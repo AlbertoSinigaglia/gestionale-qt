8ccd597e547704da8e15edac660ccaf7fe198ced
AlbertoSinigaglia <alda090299>
2020-08-11 23:25:32 +0200

    creazione modello

---
+++ employeesmanagement.cpp
@@ -0,0 +1,6 @@
+#include "employeesmanagement.h"
+
+EmployeesManagement::EmployeesManagement()
+{
+
+}
---
+++ employeesmanagement.h
@@ -0,0 +1,11 @@
+#ifndef EMPLOYEESMANAGEMENT_H
+#define EMPLOYEESMANAGEMENT_H
+
+
+class EmployeesManagement
+{
+public:
+    EmployeesManagement();
+};
+
+#endif // EMPLOYEESMANAGEMENT_H
