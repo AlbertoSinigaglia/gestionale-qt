6ad8b1d04b931ff909dfa121f888d94221b7bf78
AlbertoSinigaglia <sinigagliaalberto@gmail.com>
2020-08-28 17:16:11 +0200

    impostato lingua applicazione a italiano (principalmente per i bottoni dei messagebox)

--- main.cpp
+++ main.cpp
@@ -4,10 +4,22 @@
 #include "controller.h"
 #include "CSV/CSVWriter.h"
 #include "collections/DynamicArray.h"
-
+#include<QTranslator>
+#include<QLibraryInfo>
 
 int main(int argc, char *argv[]){
     QApplication a(argc, argv);
+    QTranslator qtTranslator;
+    QLocale qloc(QLocale("it_IT"));
+    QLocale::setDefault(qloc);
+    if (qtTranslator.load(qloc ,QString("qt"), QString("_"),QLibraryInfo::location(QLibraryInfo::TranslationsPath))){
+        a.installTranslator(&qtTranslator);
+    }
+
+    QTranslator qtBaseTranslator;
+    if (qtBaseTranslator.load("qtbase_" + QLocale::system().name(),QLibraryInfo::location(QLibraryInfo::TranslationsPath))){
+        a.installTranslator(&qtBaseTranslator);
+    }
     a.setWindowIcon(QIcon(":resources/logo.ico"));
     a.setApplicationName("MyGestionale");
     Controller w;
